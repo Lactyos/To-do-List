43f5335e3b02998ec879ebd4c846a5e822de9c12
Lactyos <81819663+Lactyos@users.noreply.github.com>
2021-07-11 15:19:49 +0100

    Add files via upload

---
+++ program.js
@@ -0,0 +1,44 @@
+function setThing() {
+
+    const input = document.getElementById("input_box").value;
+
+    document.getElementById("input_box").value = "";
+
+    // Lista
+    const create = document.createElement("li");
+    const text = document.createTextNode(input);
+    create.appendChild(text);
+
+    const element = document.getElementById("list");
+    element.appendChild(create);
+
+    // Button
+    const button = document.createElement("button");
+    const buttonText = document.createTextNode("Clear");
+    button.appendChild(buttonText);    
+    create.appendChild(button);
+    
+    //Single Remove
+    button.addEventListener("click", function(){
+        create.remove();
+    })
+        
+}
+
+
+function removeAll() {
+    document.getElementById("list").innerHTML = "";
+}
+
+document.addEventListener("keypress", (event) => {
+    if (event.key == "Enter") {
+        setThing();
+    }
+});
+
+
+
+
+
+
+    
